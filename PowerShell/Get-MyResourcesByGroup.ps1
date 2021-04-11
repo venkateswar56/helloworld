e6a7799977316fec477be3b93e8e94418203b841
venkateswar56 <52025830+venkateswar56@users.noreply.github.com>
2021-04-11 11:55:23 +0000

    Adding Powershell modules and scripts to get My Resources (#5)

---
+++ PowerShell/Get-MyResourcesByGroup.ps1
@@ -0,0 +1,7 @@
+﻿$Resources = @()
+$groups = Get-AzureRmResourceGroup
+
+foreach ($group in $groups) {
+    $Resources = Get-MyResources -GroupName $group.ResourceGroupName
+    $Resources | Format-Table
+ }
